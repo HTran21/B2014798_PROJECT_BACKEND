0eb0416edf0d94849c2289e39b07d9f5ae2208ad
HTran21 <tranhoangtran22225@gmail.com>
2023-11-09 10:39:16 +0700

    Them nhan vien

--- src/app/controller/AuthenticationController.js
+++ src/app/controller/AuthenticationController.js
@@ -1,4 +1,5 @@
 const KhachHang = require('../models/KhachHang');
+const NhanVien = require('../models/NhanVien')
 const { multipleMongoseToObject } = require('../utils/mongose');
 const userServices = require('../../services/userService');
 const multer = require('multer');
@@ -26,6 +27,63 @@ class Authentication {
 
                     const AnhDaiDien = req.file.originalname;
 
+                    const existingUser = await NhanVien.findOne({ SoDienThoai });
+
+                    if (existingUser) {
+                        // return res.status(400).json({ error: "Người dùng đã tồn tại" })
+                        return res.json({ error: "Người dùng đã tồn tại" })
+                    }
+                    else {
+
+                        const newKhachHang = new NhanVien({
+                            HoTenKH,
+                            Password: hashedPassword,
+                            DiaChi,
+                            SoDienThoai,
+                            AnhDaiDien,
+                        })
+
+                        await newKhachHang.save();
+
+                        return res.json({ message: 'Đăng ký người dùng thành công', data: req.body });
+                    }
+
+                }
+                catch (error) {
+                    console.log('Lỗi khi đăng ký người dùng', error);
+                    res.status(500).json({ message: 'Lôi khi đăng ký người dùng' })
+                }
+
+
+            }
+        })
+
+
+
+    }
+
+    async createStaff(req, res, next) {
+
+        const upload = multer({ storage: storage }).single("avatar");
+
+        upload(req, res, async function (err) {
+            if (err instanceof multer.MulterError) {
+                return res.status(400).json({ error: 'Lỗi tải lên tệp' });
+            }
+            else if (err) {
+                return res.status(500).json({ error: 'Lỗi tải lên tệp' });
+            }
+            else {
+                try {
+                    const HoTenKH = req.body.username;
+                    const Password = req.body.password;
+                    const SoDienThoai = req.body.phone;
+                    const ChucVu = req.body.position;
+                    const DiaChi = req.body.address;
+                    const hashedPassword = await userServices.hashPassword(Password);
+
+                    const AnhDaiDien = req.file.originalname;
+
 
 
 
@@ -37,17 +95,18 @@ class Authentication {
                     }
                     else {
 
-                        const newKhachHang = new KhachHang({
+                        const newNhanVien = new NhanVien({
                             HoTenKH,
                             Password: hashedPassword,
                             DiaChi,
                             SoDienThoai,
+                            ChucVu,
                             AnhDaiDien,
                         })
 
-                        await newKhachHang.save();
+                        await newNhanVien.save();
 
-                        return res.json({ message: 'Đăng ký người dùng thành công', data: req.body })
+                        return res.json({ message: 'Đăng ký người dùng thành công', data: req.body });
                     }
 
                 }
@@ -69,6 +128,29 @@ class Authentication {
         const Password = req.body.password;
         try {
             const existingUser = await KhachHang.findOne({ SoDienThoai });
+            if (!existingUser) {
+                return res.send({ error: "Tài khoản không tồn tại" })
+            }
+            else {
+                const checkPassword = await userServices.checkPassword(Password, existingUser.Password);
+                if (!checkPassword) {
+                    return res.send({ error: "Sai mật khẩu" })
+                }
+                else {
+                    return res.send({ message: "Đăng nhập thành công", data: existingUser })
+                }
+            }
+        } catch (error) {
+            console.log('Lỗi khi đăng nhập', error);
+            res.status(500).json({ message: 'Lôi khi đăng nhập' })
+        }
+    }
+
+    async loginStaff(req, res, next) {
+        const SoDienThoai = req.body.phone;
+        const Password = req.body.password;
+        try {
+            const existingUser = await NhanVien.findOne({ SoDienThoai });
             if (!existingUser) {
                 return res.send({ error: "Tài khoản không tồn tại" })
             }
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const authenticationController = require('../app/controller/AuthenticationController');
 
 router.post("/", authenticationController.createUser);
+router.post("/staff", authenticationController.createStaff);
 router.post("/login", authenticationController.login);
+router.post("/loginStaff", authenticationController.loginStaff);
 router.put("/edit", authenticationController.editProfile);
 router.get("/logout", authenticationController.logout);
 router.get("/info", authenticationController.inforUser);
